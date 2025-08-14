4fa094191b7053463cece92835b48a0abafe8973
Joseph Kawe <kohako.tech@gmail.com>
2025-08-14 04:54:43 -0300

    docs: add dependencies

---
+++ client/cypress.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 5000,
+    video: false,
+    screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        }
+      });
+
+      return config;
+    },
+  },
+  env: {
+    apiUrl: "https://api.chucknorris.io"
+  }
+});
